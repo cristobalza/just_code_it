02b406c0e3dec9755109ea4091710c02873133c9
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2022-11-29 01:03:45 -0300

    Time: 913 ms (68.59%), Space: 22.8 MB (52.59%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -4,19 +4,21 @@ class Solution:
         
         dp = [[False for _ in range(len(haystack) + 1) ] for _ in range(len(needle) + 1)]
         dp[0][0] = True
-        for j in range(1, len(haystack) + 1):
-            if haystack[j-1] != '*':
-                break
-            dp[0][j] = True
         
+        for c in range(1, len(haystack) + 1):
+            if haystack[c-1] == '*':
+                dp[0][c] = True
+            else:
+                break
+            
         for r in range(1, len(needle) + 1):
             for c in range(1, len(haystack) + 1):
-                if haystack[c-1] == '?' or needle[r-1] == haystack[c-1]:
-                    dp[r][c] = dp[r-1][c-1]
-                elif p[c-1] == '*':
+                if p[c-1] == '*':
                     dp[r][c] = dp[r-1][c] or dp[r][c-1]
+                elif haystack[c-1] == '?' or needle[r-1] == haystack[c-1]:
+                    dp[r][c] = dp[r-1][c-1]
                 else:
-                    continue
+                    continue 
         
         return dp[-1][-1]
         
