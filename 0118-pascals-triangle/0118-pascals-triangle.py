6d2e18afcb8020c6f3c39707a7df849312b656c0
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2022-12-02 01:55:46 -0300

    Time: 57 ms (48.58%), Space: 13.8 MB (97.94%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res = []
-        subset = [1]
+        dp = [1]
         while len(res) < numRows:
-            prev = subset
-            res.append(prev)
-            subset = []
+            prev = dp # prev row
+            dp = [1] * (len(prev)+1) # new
             for i in range(len(prev) + 1):
                 if i == 0 or i == len(prev):
-                    subset.append(1)
+                    continue
                 else:
-                    subset.append(prev[i-1] + prev[i])
+                    dp[i] = prev[i - 1] + prev[i]
+            res.append(prev)
         return res
             
             
