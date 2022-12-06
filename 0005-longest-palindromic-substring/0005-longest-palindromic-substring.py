304c5c1e0f7677498b7bb7347a144ca78d893e37
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2022-12-06 18:29:25 -0300

    Time: 5720 ms (14.44%), Space: 22 MB (10.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -7,16 +7,21 @@ class Solution:
         for i in range(n):
             dp[i][i] = True
             
+        # iterate backwards
+        # iterate forward with respect to the backward index (O(n^2))
         res = s[-1]
         for i in range(n-1, -1, -1):
             for j in range(i+1, n):
-                if s[i] == s[j]:
-                    if j - i == 1 or dp[i+1][j-1] is True:
-                        dp[i][j] = True
-                        word = s[i:j+1]
-                        # print(f'i: {i}, j: {j}, -> {dp}')
-                        # print(f'word: {word}')
-                        if len(res) < len(word):
-                            res = word
+                # 1) Check if chars at end points are the same
+                # 2) Check either:
+                #    - If one char word
+                #    - Use dp to see mid is in diagonal. Iteration will expand Ts for longer words
+                if s[i] == s[j] and (j - i == 1 or dp[i+1][j-1] is True):
+                    dp[i][j] = True
+                    word = s[i:j+1]
+                    # print(f'i: {i}, j: {j}, -> {dp}')
+                    # print(f'word: {word}')
+                    if len(res) < len(word):
+                        res = word
         return res   
         
