87729fd5dda882e1b0fb83b55978d8223d4e9c0a
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2022-12-07 04:19:36 -0300

    Time: 5608 ms (14.83%), Space: 21.9 MB (10.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,22 +6,19 @@ class Solution:
         # diagonal of T
         for i in range(n):
             dp[i][i] = True
-            
+        
+        res = s[-1]
         # iterate backwards
         # iterate forward with respect to the backward index (O(n^2))
-        res = s[-1]
-        for i in range(n-1, -1, -1):
-            for j in range(i+1, n):
-                # 1) Check if chars at end points are the same
-                # 2) Check either:
-                #    - If one char word
-                #    - Use dp to see mid is in diagonal. Iteration will expand Ts for longer words
-                if s[i] == s[j] and (j - i == 1 or dp[i+1][j-1] is True):
-                    dp[i][j] = True
-                    word = s[i:j+1]
-                    # print(f'i: {i}, j: {j}, -> {dp}')
-                    # print(f'word: {word}')
-                    if len(res) < len(word):
-                        res = word
+        for back in range(n - 1, -1, -1):
+            for fwd in range(back, n):
+                # print(f"fwd: {s[fwd]}")
+                # print(f"back: {s[back]}")
+                if s[fwd] == s[back] :
+                    if (back + 1 < n and fwd - 1 >= 0 and dp[back + 1][fwd - 1] is True) or ( fwd - back == 1):
+                        dp[back][fwd] = True
+                        word = s[back:fwd+1]
+                        if len(word) > len(res):
+                            res = word
         return res   
         
