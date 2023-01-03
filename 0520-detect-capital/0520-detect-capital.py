dbd61dd57d5043b141d1c7456ab169ddd3af1d67
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-02 18:22:14 -0600

    Time: 80 ms (5.66%), Space: 13.8 MB (56.08%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -1,20 +1,5 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        n = len(word)
-        if n == 1:
-            return True
-        
-        if word[0].isupper() and word[1].isupper():
-            for i in range(2, n):
-                if not word[i].isupper():
-                    return False
-        elif word[0].isupper() and not word[1].isupper():
-             for i in range(2, n):
-                if word[i].isupper():
-                    return False
-        elif not word[0].isupper():
-            for i in range(1, n):
-                if word[i].isupper():
-                    return False
-        return True
+        import re
+        return re.fullmatch(r"[A-Z]*|.[a-z]*", word)
             
