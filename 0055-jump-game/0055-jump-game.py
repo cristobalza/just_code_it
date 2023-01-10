85ea3d927e9b5318477178d00d30e91d109fb3c3
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-10 15:16:17 -0600

    Time: 525 ms (58.78%), Space: 15.2 MB (80.07%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,12 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        dp = [False] * n
-        dp[n - 1] = True
-        for i in range(n - 2, -1, -1):
-            # print(f'pre :{dp} i: {i}, i+nums[i]+1: {i+nums[i]+1}')
-            dp[i] = any(dp[i:i+nums[i]+1])
-            # print(f'post:{dp}')
+        if n == 1: return True
+        dp = [0] * n
+        dp[0] = nums[0]
+        if dp[0] == 0: return False
+        for i in range(1, n):
+            if dp[i-1] == 0:
+                return False
+            dp[i] = max(dp[i-1] - 1, nums[i])
 
-        return dp[0]
+        return any(dp[-2:]) > 0
+        
         
