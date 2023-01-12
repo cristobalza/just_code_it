7e6d1c7d5f971868823c3cafe01b43cbd0bbf4f2
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-11 20:10:13 -0600

    Time: 674 ms (93.35%), Space: 71.3 MB (75.96%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,38 +1,27 @@
-
-        
 class TimeMap:
 
     def __init__(self):
-        self.hm = collections.defaultdict(list)
-
+        self.hm = collections.defaultdict(list[tuple])
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         self.hm[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        # binary search
-        
+        if key not in self.hm: return ""
         arr = self.hm[key]
-        n= len(arr)
-        l, r = 0,  n
-        
-        if n == 0: return ""
-
-        if timestamp < arr[0][0]: return ""
-        
+        n = len(arr)
         if timestamp >= arr[n-1][0]: return arr[n-1][1]
-        
-        
-        
-        while l< r:
-            mid = (l +r) // 2
+        if timestamp < arr[0][0]: return ""
+        l, r = 0, n-1
+        while l < r:
+            mid = (l+r) // 2
             if arr[mid][0] <= timestamp:
-                l = mid  + 1
+                l = mid + 1
             else:
-                r = mid 
+                r = mid
+        return arr[r-1][1]
         
-        return arr[r - 1][1] 
-            
 
 
 # Your TimeMap object will be instantiated and called as such:
