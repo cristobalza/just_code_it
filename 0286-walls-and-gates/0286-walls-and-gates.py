50da5dc3c0dc0218f0d52bf3ba455d146b25112c
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-14 16:58:15 -0600

    Time: 255 ms (90.63%), Space: 16.9 MB (83.67%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,26 +3,21 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        INF = 2**31 - 1
         m, n = len(rooms), len(rooms[0])
-        check=lambda r,c: 0<= r < m and 0<= c < n and rooms[r][c] != -1 and rooms[r][c] != 0
-        # collect gates
         q = collections.deque()
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        check = lambda r,c: 0<=r<m and 0<=c<n and rooms[r][c] != 0 and rooms[r][c] != -1
         for r in range(m):
             for c in range(n):
                 if rooms[r][c] == 0:
                     q.append((r,c))
-        # bfs
         while q:
-            r,c = q.popleft()
-            dist = rooms[r][c] + 1
-            for x, y in [(1,0), (-1,0), (0,1), (0,-1)]:
-                if not check(r+x, c+y):
-                    continue
-                if dist < rooms[r+x][c+y]:
-                    rooms[r+x][c+y] = dist
-                    q.append((r+x, c+y))
+            r, c = q.popleft()
+            dist = 1 + rooms[r][c]
+            for x, y in directions:
+                adj_r, adj_c = r+x, c+y
+                if check(adj_r, adj_c) and dist < rooms[adj_r][adj_c]:
+                    q.append((adj_r, adj_c))
+                    rooms[adj_r][adj_c] = dist
         
-                    
                 
-                    
