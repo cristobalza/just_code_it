fc757a504ef601a471dc820aefd85c6de318464f
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-13 19:14:43 -0600

    Time: 24 ms (97.97%), Space: 14 MB (15.77%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,23 +1,8 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        '''
-        x = 2 , n = 3 => 8
-        
-        1------------------1024
-        
-        1-------------8
-                   
-        
-        '''
-        res = self.helper(x,abs(n)) if x != 1 else 1
-        return res if n >= 0 else 1/res
-        
-    def helper(self, x, n):
-        if n == 0:
-            return 1
-        if n == 1:
-            return x
-        return self.helper((x*x), n // 2) if n % 2 == 0 else x * self.helper(x, n - 1)
-      
-            
-        
+        def recursion(x, exp):
+            if x == 1 or exp == 0: return 1
+            if x == 0: return 0
+            if exp == 1: return x
+            return recursion(x * x,   exp //2) if exp % 2 == 0 else recursion(x, exp - 1 ) * x
+        return recursion(x, n) if n>0 else 1/ recursion(x, abs(n))
