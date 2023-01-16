05a68972e3b4abad122b1d34cd421efe1e432e70
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-16 16:30:36 -0600

    Time: 32 ms (86.08%), Space: 13.8 MB (98.90%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,15 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [0] * (n+1)
+        dp = [0]*(n+1)
         dp[-1] = 1
-        
-        for i in range(n - 1,  -1, -1):
+        check=lambda num: 1<= num <= 26
+        for i in range((n+1)-2, -1, -1):
             if s[i] == '0':
                 continue
-            if i + 2 <= n and int(s[i:i+2]) <= 26:
-                dp[i] += dp[i+2]
-            if i + 1 <= n: # given that it will be less than 26 since only one digit
+            if i + 1 < (n+1):
                 dp[i] += dp[i+1]
-                
+            if i + 2 < (n+1) and check(int(s[i:i+2])):
+                dp[i] += dp[i+2]
         return dp[0]
