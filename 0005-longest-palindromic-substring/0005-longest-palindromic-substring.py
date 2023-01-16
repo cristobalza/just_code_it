f90e5cc732fe760a260e0891ece28bfd826a2df2
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-15 18:33:09 -0600

    Time: 6551 ms (12.62%), Space: 21.9 MB (11.97%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n= len(s)
+        n = len(s)
         dp = [[False] * n for _ in range(n)]
-        
-        # diagonal of T
+        res = ''
+        check=lambda b,f: b + 1 < n and f - 1 >= 0 and dp[b + 1][f - 1] == True
+        # diagonal 
         for i in range(n):
             dp[i][i] = True
-        
-        res = s[-1]
-        # iterate backwards
-        # iterate forward with respect to the backward index (O(n^2))
-        for back in range(n - 1, -1, -1):
-            for fwd in range(back, n):
-                # print(f"fwd: {s[fwd]}")
-                # print(f"back: {s[back]}")
-                if s[fwd] == s[back] :
-                    if (back + 1 < n and fwd - 1 >= 0 and dp[back + 1][fwd - 1] is True) or ( fwd - back + 1 <= 2):
-                        dp[back][fwd] = True
-                        if fwd - back + 1 > len(res):
-                            res = s[back:fwd+1]
-        return res   
-        
+        for back in range(n-1, -1, -1):
+            for fwd in range(n):
+                if s[back] == s[fwd] and (check(back, fwd) or fwd - back + 1 <= 2):
+                    dp[back][fwd] = True
+                    if fwd - back + 1 > len(res):
+                        res = s[back:fwd+1]
+        return res
+                
