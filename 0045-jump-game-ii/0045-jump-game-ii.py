c337a883ce3d9341d23567befaa58ce43ee37b37
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-01-21 16:37:24 -0600

    Time: 11619 ms (9.17%), Space: 15 MB (89.86%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,7 +5,7 @@ class Solution:
         dp[0] = 0
         for i in range(n):
             for j in range(0, nums[i] + 1):
-                if i + j < n:
-                    dp[i+j] = min(dp[i+j], dp[i] + 1)
-                    
+                if i + j == n:
+                    break
+                dp[i+j] = min(dp[i+j], dp[i] + 1)
         return dp[-1]
