0597d9bb9e683f033bdc754d47fe0fcdee9e2d29
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-02-23 16:05:11 -0600

    Time: 135 ms (58.91%), Space: 14.7 MB (66.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,21 +3,31 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        stack = []
         m, n = len(matrix), len(matrix[0])
+        isRowZeroAZero = False
         for r in range(m):
             for c in range(n):
                 if matrix[r][c] == 0:
-                    stack.append((r,c))
+                    matrix[0][c] = 0
+                    
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        isRowZeroAZero = True
+                    
         
-        while stack:
-            r, c = stack.pop()
-            self.helper(matrix, r, c, m, n)
-            
-    def helper(self, matrix, r, c, m, n):
-        for i in range(m):
-            matrix[i][c] = 0
-        for j in range(n):
-            matrix[r][j] = 0
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+                    
+        if matrix[0][0] == 0:
+            for r in range(m):
+                matrix[r][0] = 0
+                
+        if isRowZeroAZero:
+            for c in range(n):
+                matrix[0][c] = 0
         
-            
+                
+                
