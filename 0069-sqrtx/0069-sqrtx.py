1c208bb58d170143c597d93bcebd15bdfb90008b
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-02-23 11:16:47 -0600

    Time: 41 ms (59.82%), Space: 13.9 MB (7.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,14 +1,34 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        '''
+        x return sqrt(x)
+        
+        x = 4 -> 2
+        
+        4 ** 1/2 = 2
+        4 = 2 ** 2
+        4 = 2 * 2
+        
+        
+        x = 64
+     64 ** 1/ 2 = 8
+     
+     
+        0 ---32---- 64
+        l   m     r
+        '''
+        
         if x < 2: return x
         l, r = 0, x
         while l < r:
             mid = (l + r) // 2
-            if mid * mid <= x < (mid+1) * (mid+1):
+            if mid * mid <= x < (mid + 1) * (mid+1):
                 return mid
-            if mid * mid < x:
+            elif mid * mid < x:
                 l = mid + 1
             else:
                 r = mid
+                
+        
         
         
