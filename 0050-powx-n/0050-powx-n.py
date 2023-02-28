a29072332ec04c414be04fe8d8900567cd2ab236
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-02-27 19:30:43 -0600

    Time: 37 ms (36.99%), Space: 13.8 MB (56.15%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,8 +1,11 @@
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        def recursion(x, exp):
-            if x == 1 or exp == 0: return 1
-            if x == 0: return 0
-            if exp == 1: return x
-            return recursion(x * x,   exp //2) if exp % 2 == 0 else recursion(x, exp - 1 ) * x
-        return recursion(x, n) if n>0 else 1/ recursion(x, abs(n))
+    def myPow(self, x: float, n: int) -> float:        
+        memo = {}
+        def helper(x, n):
+            if n == 1: return x
+            if n == 0: return 1
+            if x in memo: return memo[x]
+            memo[x] = helper(x*x, n // 2) if n % 2 == 0 else x * helper(x, n-1)
+            return memo[x]
+        return helper(x, n) if n > 0 else 1 / helper(x, abs(n))
+    
