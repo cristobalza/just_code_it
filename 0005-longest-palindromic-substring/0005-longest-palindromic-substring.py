ed254ddc6869e3b305c3a60a87a1deb881138d00
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-03-06 09:05:28 -0600

    Time: 9636 ms (5.00%), Space: 21.9 MB (18.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,15 +3,15 @@ class Solution:
         n = len(s)
         dp = [[False] * n for _ in range(n)]
         res = s[-1]
-        check = lambda r, c : (r + 1 < n and c-1 >= 0 and dp[r + 1][c - 1] == True) or c - r + 1 <=2
-        for i in range(n):
-            dp[i][i] = True
+        check = lambda r, c : s[r] == s[c] and  ((r + 1 < n and c-1 >= 0 and dp[r + 1][c - 1] == True) or c - r + 1 <=2)
         for r in range(n - 1, -1, -1):
             for c in range(n):
-                if s[r] == s[c] and check(r, c):
+                if r == c:
+                    dp[r][c] = True
+                elif check(r, c):
                     dp[r][c] = True
                     # check update
-                    if c-r + 1 > len(res):
+                    if c - r + 1 > len(res):
                         res = s[r:c +1]
         return res
         
