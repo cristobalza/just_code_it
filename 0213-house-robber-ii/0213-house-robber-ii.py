2b5674206ada60c304e139bab75d1039f347871f
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2023-03-06 20:29:55 -0600

    Time: 37 ms (41.85%), Space: 13.9 MB (60.80%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,22 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return max(nums)
         n = len(nums)
-        fwd_dp = [0] * (n)
-        fwd_dp[1] = nums[1]
-        fwd_dp[2] = max(nums[1], nums[2])
+        if n <= 2: return max(nums)
+        dp1= [0] * n
+        dp2= [0] * (n-1)
+        dp1[1], dp1[2] = nums[1], max(nums[1], nums[2])
+        dp2[0], dp2[1] = nums[0], max(nums[0], nums[1])
         for i in range(3, n):
-            fwd_dp[i] = max(fwd_dp[i-1], nums[i] + fwd_dp[i-2])
-            
-            
-        back_dp = [0] * (n - 1)
-        back_dp[0] = nums[0]
-        back_dp[1] = max(nums[0], nums[1])
+            dp1[i] = max(dp1[i-1], dp1[i-2] + nums[i])
         for i in range(2, n - 1):
-            back_dp[i] = max(back_dp[i-1], nums[i] + back_dp[i-2])
-        
-        return max(back_dp[-1], fwd_dp[-1])
-        
-        
-        
+            dp2[i] = max(dp2[i-1], dp2[i-2] + nums[i])
+        return max(dp1[-1], dp2[-1])
+            
