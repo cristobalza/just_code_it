95fc540b6b1d7f0b97ad1fe835d659b86c64bfbf
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2024-01-25 23:07:05 -0600

    Time: 379 ms (68.20%), Space: 17.9 MB (58.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         if len(nums) == 1: return True
-        if nums[0] == 0: return False
         memo = [0] * len(nums)
         memo[0] = nums[0]
         for i in range(1, len(nums)):
-            if memo[i-1] == 0:
-                return False
-            memo[i] = max(memo[i-1] - 1, nums[i])
+            if memo[i-1] > 0:
+                memo[i] = max(memo[i-1] - 1, nums[i])
         return any(memo[-2:]) > 0
+        
