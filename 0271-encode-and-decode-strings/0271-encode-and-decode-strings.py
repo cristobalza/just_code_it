b0f3ae9659db3610a9a0e3fbac1d06ea4925afc8
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2024-02-10 22:35:02 -0600

    Time: 72 ms (48.13%), Space: 17 MB (59.91%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,35 +2,30 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
+        '''
+        ['dog, 'banana', 'orange', 'apple']
+        '6?banana3?dog'
+        '''
         res = ""
-        for s in strs:
-            res += f"{len(s)}?{s}"
+        for _str in strs:
+            res += f"{len(_str)}?{_str}"
         return res
+            
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        '''
-       s =  Hello?World?
-            i
-                 j
-        
-        ''' 
-        
         res = []
         i = 0
         while i < len(s):
             j = i
             while j < len(s) and s[j] != '?':
-                j += 1
+                j+=1
             size = int(s[i:j])
-            word = s[j + 1: j + 1 + size]
-            res.append(word)
-            i = j + 1 + size
+            res.append(s[j+1:j+1+size])
+            i = j+1+size
         return res
-        
-                        
         
 
 
