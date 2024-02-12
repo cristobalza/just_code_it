771fadc59cc113d46f10411533d13c20978cd0c0
Cristobal Zamorano Astudillo <cristobalzamas@gmail.com>
2024-02-11 21:23:35 -0600

    Time: 48 ms (51.94%), Space: 17.2 MB (64.32%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # binary search horizontal and vertical
+        m,  n = len(matrix), len(matrix[0])
+        
+        l, r = 0, m - 1
+        find_row = -1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[mid][0] <= target <= matrix[mid][n - 1]:
+                find_row = mid
+                break
+            elif matrix[mid][0] > target:
+                r = mid - 1
+            elif matrix[mid][n - 1] < target:
+                l = mid + 1
+    
+        if find_row == -1:
+            return False
+        
+        arr = matrix[find_row]
+        # print(arr)
+        
+        l, r = 0, n - 1
+        while l < r:
+            mid = (l + r) // 2
+            if arr[mid] < target:
+                l = mid + 1
+            else:
+                r = mid
+        
+        return True if arr[r] == target else False
+        
